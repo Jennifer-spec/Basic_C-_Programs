d5d14c465370a33df20daafd86251dd0679927cf
Jennifer-spec <fuqinghua2@gmail.com>
2023-08-06 21:45:36 -0700

    add a new project called ShippingQuote

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please input the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please input the package's width:");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please input the package's height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please input the package's length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+                if ((width + height + length) > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");// the program is over
+                }
+                else
+                {
+                    int product = (width * height * length * weight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is:$" + product);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                    //the program is over here
+                }
+            }
+        }
+    }
+}
