dff254e943ee541a9ab33900ca9bc8f0c32fc828
Jennifer-spec <fuqinghua2@gmail.com>
2023-08-06 22:59:25 -0700

    add a new project called StringOperation

---
+++ StringsOperation/StringsOperation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace StringsOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st1 = "My name ";
+            string st2 = "is ";
+            string st3 = "Jennifer.";
+            string wholest = st1 + st2 + st3;
+            Console.WriteLine(wholest);
+           // Console.ReadLine();
+
+            string name = "Jennifer";
+            Console.WriteLine(name.ToUpper());
+            Console.ReadLine();
+
+            //stringBuilder sb = new stringBuilder();
+            //sb.Append("My name is Jennifer");
+            //Console.WriteLine(sb);
+
+        }
+    }
+}
